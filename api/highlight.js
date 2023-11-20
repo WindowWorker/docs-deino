f2b2f922216fcc9465a2d7bee17afd6db83a9004
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-20 06:30:06 +0000

    asdf

--- api/highlight.js
+++ api/highlight.js
@@ -103,6 +103,14 @@ if(window.location.href.includes('/docs/handbook/tsconfig-json.html')){return;}*
     if(codes[i].hasAttribute('highlight-count')){
       hlc=parseInt(codes[i].getAttribute('highlight-count'));
       if(hlc>=5){
+        codes[i].setAttribute('highlighted','true');
+        if(codes[i].firstElementChild){
+          codes[i].firstElementChild.appendChild(document.createElement('highlight-me'));
+          
+        }else{
+          codes[i].appendChild(document.createElement('highlight-me'));
+        }
+        highlighter();
         continue;
       }else{
         hlc++;
@@ -215,6 +223,25 @@ document.body.appendChild(m);
 
 
   }
+
+ /* setInterval(function(){
+  let doublecodes=document.querySelectorAll('code>code');
+  let doublecodes_length=doublecodes.length;
+  for(let i=0;i<doublecodes_length;i++){try{
+
+    doublecodes[i].parentElement.innerHTML=doubleCodes[i].innerHTML;
+
+  }catch(e){continue;}}
+
+    doublecodes=document.querySelectorAll('code>*>code');
+    doublecodes_length=doublecodes.length;
+    for(let i=0;i<doublecodes_length;i++){try{
+
+      doublecodes[i].parentElement.parentElement.innerHTML=doubleCodes[i].innerHTML;
+
+    }catch(e){continue;}}
+
+  },100);*/
 
 
 }
