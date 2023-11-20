383a268cd26c59acf44bf43058e3598eba2ed3e2
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-20 02:19:36 +0000

    asfd

--- api/highlight.js
+++ api/highlight.js
@@ -26,8 +26,9 @@ function Highlight(){
     let startH_length=startH.length;
     for(let i=0;i<startH_length;i++){
 
-      if((startH[i].children.length<2)
-      &&(startH[i].innerText.includes(R))){
+      if((startH[i].children.length>2)
+      &&(startH[i].innerText.includes(R))
+      &&(startsH[i].innerText.trim().length>50)){
         startH[i].removeAttribute('highlighted');
       }
 
